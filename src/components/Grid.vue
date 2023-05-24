b7385411a39d38b18124cd568eadb56f501c73fd
Karol Mąkowski <karol.makowski@cloudflight.io>
2023-05-24 17:02:40 +0200

    Change grid bg color

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -87,7 +87,7 @@ const handleMouseUp = () => {
   display: grid;
   grid-template-columns: repeat(var(--cols), minmax(1rem, 1fr));
   grid-template-rows: repeat(var(--cols), minmax(1rem, 1fr));
-  background-color: rgba(dimgray, 0.25);
+  background-color: rgba(dimgray, 0.3);
 
   &__field {
     display: flex;
